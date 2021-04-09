9fc47cce1cc0618bdfa4d9f9bf7f35bd6aeb3f99
shitianshu <doglooksgood@gmail.com>
2021-04-09 09:22:03 +0800

    update

--- pdump.el
+++ pdump.el
@@ -2,14 +2,10 @@
 (load (expand-file-name "early-init.el" user-emacs-directory))
 (load (expand-file-name "init.el" user-emacs-directory))
 
-;;; Some packages to exclude during dump.
-(setq +pdump-exclude-packages '(rime telega))
-
 ;;; Ensure every installed package is loaded.
 (+measure-time-1
  (dolist (pkg +pdump-packages)
-   (unless (member pkg +pdump-exclude-packages)
-     (require pkg))))
+   (require pkg)))
 
 ;;; We have to unload tramp in pdump, otherwise tramp will not work.
 (tramp-unload-tramp)
